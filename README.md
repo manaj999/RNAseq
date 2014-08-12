daeaf2346a4d47e72c0dc7269503e6c85a202469
Manoj Kanagaraj <manaj999@gmail.com>
2014-08-12 13:23:09 -0700

    Updated README

--- README.md
+++ README.md
@@ -14,3 +14,51 @@ submit_2.sh
 
 cn_hold.sh
 cummeRpipe.r
+
+----------------------------------------------------------------------------------
+USAGE:
+	perl rna_seq_pipeline.pl -i <INPUT> -o <OUTPUT> -g <GENOME> -r <RUNID> <OPTIONS>
+
+EXAMPLE RUN:
+	perl rna_seq_pipeline.pl -i /home/kanagarajm/samples_fq/ -o /mnt/state_lab/share/Manoj/rna_seq_out/ -g u -r 81214 --pairedEnd
+
+REQUIRED ARGUMENTS:
+	-i (input)				Directory containing all fastq files to be run through pipeline
+	-o (output)				Directory where all output files will be organized and saved
+	-g (genome build)			Genome build to be used 
+							"u" for UCSC
+							"e" for Ensembl
+							"n" for NCBI
+							"g10" for Gencode v10
+							"g19" for Gencode v19
+							"m2" for Gencode m2
+								If using --altAnnotation option, then specify PATH of directory 
+								containing necessary files for building transcriptome here instead
+	-r (runID)				Unique runID used to identify and organize outputs from a given run
+
+OPTIONS:
+	--nocuffmerge			Use to skip running cuffmerge
+	--altAnnotation			Use a different genome assembly. Specify directory where "Sequence" 
+							and "Annotation" folders are located in -g argument
+	--nodiscovery			Use to skip gene/transcript discovery and only quantify reference annotation
+	--pairedEnd 			Use for if sequencing reads are paired-end, as opposed to single-end
+
+Description:
+	This script serves as the wrapper for the entire Tuxedo pipeline.
+	It has been written to optimize the processing of multiple FASTQ 
+	files produced via RNA sequencing by running them modularly in 
+	parallel using the Oracle Sun Grid Engine (SGE).
+	
+	Together this collection of scripts can produce detailed expression 
+	metrics and publication-ready graphs produced from cuffnorm/cuffdiff
+	and cummeRbund, respectively, with just a single line of code from
+	the command line.
+
+	Within the given output directory, this script will create subdirectories
+	that store the results of each component in the Tuxedo Suite. The output
+	directory will also contain a log file that broadly documents the progress
+	of the pipeline.
+
+	If you wish to run cuffdiff you must use the pipeline.pl script after this 
+	pipeline has completed. For more information on using cuffdiff, enter
+	"perl pipeline.pl --cd" at the commandline.
--- rna_seq_pipeline.pl
+++ rna_seq_pipeline.pl
@@ -27,8 +27,6 @@ use Getopt::Long;
 	### --nodiscovery: use to skip gene/transcript discovery and only quantify reference annotation
 	### --pairedEnd: use for paired-end sequencing reads
 
-print "Beginning pipeline...\n";
-
 my ( $input, $output, $genomeType, $runID, $overrideCM, $altAnnotation, $overrideDisc, $paired );
 
 GetOptions(	
@@ -43,7 +41,66 @@ GetOptions(
 	'pairedEnd' => \$paired
 ) or die "Incorrect input and/or output path!\n";
 
-print "Arguments loaded.\n";
+if (!$input or !$output or !$genomeType or !$runID){
+	`clear`;
+	print <<HelpDocumentation;
+USAGE:
+	perl rna_seq_pipeline.pl -i <INPUT> -o <OUTPUT> -g <GENOME> -r <RUNID> <OPTIONS>
+
+EXAMPLE RUN:
+	perl rna_seq_pipeline.pl -i /home/kanagarajm/samples_fq/ -o /mnt/state_lab/share/Manoj/rna_seq_out/ -g u -r 81214 --pairedEnd
+
+REQUIRED ARGUMENTS:
+	-i (input)				Directory containing all fastq files to be run through pipeline
+	-o (output)				Directory where all output files will be organized and saved
+	-g (genome build)			Genome build to be used 
+							"u" for UCSC
+							"e" for Ensembl
+							"n" for NCBI
+							"g10" for Gencode v10
+							"g19" for Gencode v19
+							"m2" for Gencode m2
+								If using --altAnnotation option, then specify PATH of directory 
+								containing necessary files for building transcriptome here instead
+	-r (runID)				Unique runID used to identify and organize outputs from a given run
+
+OPTIONS:
+	--nocuffmerge			Use to skip running cuffmerge
+	--altAnnotation			Use a different genome assembly. Specify directory where "Sequence" 
+							and "Annotation" folders are located in -g argument
+	--nodiscovery			Use to skip gene/transcript discovery and only quantify reference annotation
+	--pairedEnd 			Use for if sequencing reads are paired-end, as opposed to single-end
+
+Description:
+	This script serves as the wrapper for the entire Tuxedo pipeline.
+	It has been written to optimize the processing of multiple FASTQ 
+	files produced via RNA sequencing by running them modularly in 
+	parallel using the Oracle Sun Grid Engine (SGE).
+	
+	Together this collection of scripts can produce detailed expression 
+	metrics and publication-ready graphs produced from cuffnorm/cuffdiff
+	and cummeRbund, respectively, with just a single line of code from
+	the command line.
+
+	Within the given output directory, this script will create subdirectories
+	that store the results of each component in the Tuxedo Suite. The output
+	directory will also contain a log file that broadly documents the progress
+	of the pipeline.
+
+	If you wish to run cuffdiff you must use the pipeline.pl script after this 
+	pipeline has completed. For more information on using cuffdiff, enter
+	"perl pipeline.pl --cd" at the commandline.
+
+	^SCROLL UP FOR MORE INFORMATION^
+HelpDocumentation
+
+	exit;
+}
+else {
+	print "Arguments loaded.\n";
+}
+
+print "Beginning pipeline...\n";
 
 # Input checks
 ## Make sure genome type argument is valid. File path to the genome annotation directory is accepted if altAnnotation option is specified
